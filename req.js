e3ab20d10686504306502dfb8d7db1368e347ad1
fangkampanat <fangkampanat@gmail.com>
2021-02-02 13:37:59 +0700

    Update req.js

--- req.js
+++ req.js
@@ -11,16 +11,14 @@
 window.onload = function afterWebPageLoad() {
     setInterval(autoBuy, 500);
     function autoBuy() {
-        if (window.location.href.indexOf("https://thisshop.com/item/") > -1) {
-                    document.querySelector('button#buyNow.button.orange').click();
-                    isBuyNow = true;
-        } else if (window.location.href.indexOf("https://thisshop.com/app/shopcart/") > -1) {
+        if (window.location.href.indexOf("thisshop.com/item/") > -1) {
+                    document.querySelectorAll('button#buyNow.button.orange')[0].click()
+        } else if (window.location.href.indexOf("thisshop.com/app/shopcart/") > -1) {
            /* if (!isChangePM && document.getElementsByClassName('stage-term icofont ').length > 0) {
                     document.querySelectorAll('div.stage-term.icofont')[6].click();
                     isChangePM = true;
             } else { */
                     document.querySelectorAll('div.order-submit-btn.js-buy')[0].click();
-                    isBuyComplete = true;
             //}
         }
     }
