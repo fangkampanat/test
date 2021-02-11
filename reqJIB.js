da8b2469a3e0206df62a1dbf3382d8a4757ee082
fangkampanat <fangkampanat@gmail.com>
2021-02-11 20:34:26 +0700

    Update reqJIB.js

--- reqJIB.js
+++ reqJIB.js
@@ -1,21 +1,42 @@
 // ==UserScript==
 // @name JIB-Auto-Buy
 // @namespace Script Runner Pro
-// @match https://jib.co.th/web/product/product_list/* 
-// @match https://www.jib.co.th/web/product/product_list/* 
+// @match https://jib.co.th/web/cart* 
+// @match https://www.jib.co.th/web/cart* 
+// @match https://jib.co.th/web/cart/confirm/* 
+// @match https://www.jib.co.th/web/cart/confirm/* 
+// @match https://www.jib.co.th/web/cart/pay/*
+// @match https://jib.co.th/web/cart/pay/*
 // @grant none
 // ==/UserScript==
 window.onload = function afterWebPageLoad() {
+    var isPress1 = false;
+    var isPress2 = false;
+    var isPress3 = false;
+    var isPress4 = false;
     setInterval(autoBuy, 1000);
     function autoBuy() {
-        if (window.location.href.indexOf("jib.co.th/web/product/product_list/") > -1) {
-            if (document.querySelectorAll('a.buy_promo.btn-orange.text-capitalize.hidden').length > 0) {
-                    document.querySelectorAll('a.buy_promo.btn-orange.text-capitalize.hidden')[0].click();
-            }      
-        } else if (window.location.href.indexOf("jib.co.th/web/cart/confirm/") > -1) {
-            if (document.querySelectorAll('a#next_refer_confirm.btn.btn-success.btn-md').length > 0) {
-                    document.querySelectorAll('a#next_refer_confirm.btn.btn-success.btn-md')[0].click();
-            } 
-        }
+            if (document.querySelectorAll("a.btn.btn-primary.btn-md.cart-link").length > 0) {
+                if (!isPress1) {
+                    document.querySelectorAll("a.btn.btn-primary.btn-md.cart-link")[1].click();
+                    isPress1 = true;
+                }
+            } else if (document.querySelectorAll("button#btn_next.btn.btn-primary.normal").length > 0) {
+                if (!isPress2) {
+                    document.querySelectorAll("button#btn_next.btn.btn-primary.normal")[0].click();
+                    isPress2 = true;
+                }
+            } else if (document.querySelectorAll("input#order_pay_type_sub_10_menu28.order_pay_sub").length > 0) {
+                if (!isPress3) {
+                    document.querySelectorAll("input#order_pay_type_sub_10_menu28.order_pay_sub")[0].click();
+                    document.querySelectorAll("a#next_refer.btn.btn-primary.btn-md")[0].click();
+                    isPress3 = true;
+                }
+            } else if (document.querySelectorAll("span.w_font").length > 0) {
+                if (!isPress4) {
+                    document.querySelectorAll("span.w_font")[1].click();
+                    isPress4 = true;
+                }
+            }
     }
 };
